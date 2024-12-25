e7ae2d2d1d9b43b6bdbaad4d6524f6bea14b5663
BekShred <bek.schreuder@gmail.com>
2024-12-26 10:01:02 +1100

    2nd publish

--- story_content/triggers.js
+++ story_content/triggers.js
@@ -2,12 +2,6 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6G5fkEqsbhw":
-        Script1();
-        break;
-      case "6XxT542ntOj":
-        Script2();
-        break;
   }
 }
 
@@ -18,42 +12,4 @@ var object = player.object;
 var addToTimeline = player.addToTimeline;
 var setVar = player.SetVar;
 var getVar = player.GetVar;
-window.Script1 = function()
-{
-  const target = object('6RUuOiDfImr');
-const duration = 750;
-const easing = 'ease-out';
-const id = '6hQe5F7H9uZ';
-const pulseAmount = 0.03;
-player.addForTriggers(
-id,
-target.animate([
-{ scale: '1' }, { scale: `${1 + pulseAmount}` },
-{ scale: '1' }, { scale: `${1 + pulseAmount}` },
-{ scale: '1' }
-],
-  { fill: 'forwards', duration, easing }
-)
-);
-}
-
-window.Script2 = function()
-{
-  const target = object('6ADsX9MwqMU');
-const duration = 750;
-const easing = 'ease-out';
-const id = '6jbmmm9lltR';
-const pulseAmount = 0.07;
-player.addForTriggers(
-id,
-target.animate([
-{ scale: '1' }, { scale: `${1 + pulseAmount}` },
-{ scale: '1' }, { scale: `${1 + pulseAmount}` },
-{ scale: '1' }
-],
-  { fill: 'forwards', duration, easing }
-)
-);
-}
-
 };
